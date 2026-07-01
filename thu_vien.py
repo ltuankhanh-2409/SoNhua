14530ca5e597871e45c64d3e0c63d1c649f3c232
ltuankhanh-2409 <ltuankhanh@gmail.com>
2026-07-01 11:47:45 +0700

    Update thu_vien.py

--- thu_vien.py
+++ thu_vien.py
@@ -2,21 +2,13 @@ import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-SCOPE = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive"
-]
-def connect_google(ten_json,sheet_id):
-    try:
-        # Chạy trên Streamlit
-        import streamlit as st
-        creds = ServiceAccountCredentials.from_json_keyfile_dict(
-            st.secrets["gcp_service_account"],
-            SCOPE
-        )
-    except Exception as e:
-        import streamlit as st
-        st.error(f"Lỗi đọc secrets: {e}")
-        raise
+def connect_google(ten_json, sheet_id):
+    import streamlit as st
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        st.secrets["gcp_service_account"],
+        SCOPE
+    )
+
     client = gspread.authorize(creds)
     return client.open_by_key(sheet_id)
