226a92e054579941cd589146c009ef78aba2e414
ltuankhanh-2409 <ltuankhanh@gmail.com>
2026-07-02 23:13:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,10 @@ import pandas as pd
 
 dta=tv.connect_google("1ZGJ94U_I6iOb-ZInoZWTrHH4Ih7aA-ynlnrT8yR5yh8")
 #dta.sheet1.get_all_cells()
-# print(dta.sheet1)
-#  = ["cot 1","cột 2","cột 3","cột 4"]
-# .sheet1.append_row(row)
-# dta2=dta.sheet1.get_all_records()
-# print(dta2)
 st.set_page_config(page_title="📋 SỔ NHỰA",page_icon="🤣",layout="wide")
-
 st.title("📋 SỔ NHỰA")
 col1, col2, col3 = st.columns(3)
 with col1:
-    # zone= st.text_input("🔎 Line nào")
     zone = st.selectbox("🔎 Line nào",["Line 1","Line 2","Line 3","Line 4","Line 5","Line 6","Line 7"],index=5)
 with col2:
     # may = st.text_input("🔎 Nhập máy vào")
@@ -24,21 +17,14 @@ with col2:
 with col3:
     ws_mahang = dta.worksheet("Mahang")
     ds_mahang = ws_mahang.col_values(1)[1:]
-#    for mahang in dta.worksheets("Mahang"):
-#        ma_hang = mahang.col_values(1)[1:]
     ma_hang = st.selectbox("🔎 Mã hàng",ds_mahang)
 col4, col5, col6 = st.columns(3)
 with col4:
     ten_khuon = st.text_input("📋 Nhập tên Khuôn")
 with col5:
-    trai_phai = st.selectbox(" 🤚🏻 Bên tái hay phải ✋🏻",["T","P"])
+    trai_phai = st.selectbox(" 🤚🏻 Khuôn bên nào",["T","P"])
 with col6:
-
-    trong_luong = st.number_input("📋 Nhập số nhựa",min_value=1,max_value=900,value= None)
-
-# pre_data = f"{zone},{may},{ten_khuon},{trong_luong}"
-#pre_data = [zone,may,ten_khuon,trong_luong]
-
+    trong_luong = st.number_input("📋 Nhập số nhựa",min_value=1.0,max_value=900.0,value= None)
 if st.button("💾 Lưu"):
     record = dta.sheet1.get_all_records()
     if len(record) == 0:
@@ -52,8 +38,6 @@ if st.button("💾 Lưu"):
     #st.success("✅ Đã lưu thành công")
     st.toast("✅ Đã lưu thành công")
 record = dta.sheet1.get_all_records()
-#col6, col7 = st.columns(2)
-
 if  (len(record) > 0) & (trai_phai == "T"):
     df = pd.DataFrame(record)
     loc=df[
@@ -87,12 +71,3 @@ else:
         "TrongLuong": "Số nhưa phải"
     })
 st.dataframe(loc,hide_index=True,use_container_width=True)
-#        df.columns = ["Tên khuôn trái", "Trọng lượng trái"]
-
-
-# Đoạn code lấy thông tin từ Dataframe lên form nhập
-
-
-
-#st.write(df["MaHang"].unique())
-#st.write(repr(ma_hang))
