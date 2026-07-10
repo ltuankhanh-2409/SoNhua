b6dfecd4be7d2811901c1f2262c51a3ca5f358da
ltuankhanh-2409 <ltuankhanh@gmail.com>
2026-07-10 21:20:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def luu_nhua():
         (df["TenKhuon"] == ten_khuon)
         ]
     if not ten_khuon:
-        st.toast("⚠️ Bạn chưa nhập tên khuôn.")
+        st.toast(f"⚠️ Bạn chưa nhập tên khuôn {ten_khuom} đã có rồi.")
         return
     if trong_luong is None:
         st.toast("⚠️ Bạn chưa nhập số nhựa.")
